3ba930988408bdf654d90c39e70776b9229923d6
nstevanovic <nstevanovic@ryerson.ca>
2018-03-06 14:07:41 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,14 @@
-import unittest
 
-class TestMethods(unittest.TestCase):
-    def test_add(self):
-        self.assertEqual(cps3271(4), 16)
+import unittest
+import cps3271
+
+class TestMyFunc(unittest.TestCase):
+ 
+    def setUp(self):
+        pass 
+
+    def test_multiply_by_four(self): 
+        self.assertEqual(  cps3271.cps3271(4), 16)
+        
+if __name__ == '__main__':
+    unittest.main()
